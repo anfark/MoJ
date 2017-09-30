b493d0c08c3ca5b728e6e7265adf0cbff86546c4
Thomas Smits <thomsmits@gmail.com>
2017-09-30 23:38:20 +0200

    Anzeige als Balken, Reset bei Geste

--- src/main/java/de/innohacks/MoJ/Main.java
+++ src/main/java/de/innohacks/MoJ/Main.java
@@ -6,6 +6,8 @@ import de.innohacks.MoJ.motion.event.IEvent;
 import de.innohacks.MoJ.motion.event.MotionEvent;
 import org.zeromq.ZMQ;
 
+import java.util.Arrays;
+
 
 /**
  * Created by roman on 30.09.17.
@@ -22,11 +24,19 @@ public class Main {
             if (event instanceof MotionEvent) {
                 MotionEvent e = (MotionEvent)(event);
                 loc.update(e);
-                System.out.println("" + loc);
+                //System.out.println("" + loc);
             }
             else {
-                System.out.println("Received " + event);
+                //System.out.println("Received " + event);
+                loc.reset();
             }
+
+            char[] pos = new char[81];
+            Arrays.fill(pos, '-');
+
+            int index = (int) Math.abs(Math.max(Math.min(((-loc.getX() + 20.0) / 40.0) * 80, 80),0));
+            pos[index] = '|';
+            System.out.println('[' + new String(pos) + ']');
         });
 
     }
