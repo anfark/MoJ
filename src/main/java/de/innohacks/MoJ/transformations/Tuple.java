3d0bf4e9d66cd2b8bec228d2a31e1259cc87a1d9
Simon Heiss <simonheiss87@gmail.com>
2017-09-30 18:37:03 +0200

    added basic transformer for event to midi

---
+++ src/main/java/de/innohacks/MoJ/transformations/Tuple.java
@@ -0,0 +1,10 @@
+package de.innohacks.MoJ.transformations;
+
+public class Tuple<T, T1> {
+    public final T first;
+    public final T1 second;
+    public Tuple(T x, T1 y) {
+        this.first = x;
+        this.second = y;
+    }
+}
